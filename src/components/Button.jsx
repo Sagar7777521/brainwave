97b7ca8d9508b6c94c074a33c7aac22586a01b14
Sagar <70952463+Sagar7777521@users.noreply.github.com>
2024-03-14 23:37:39 +0530

    Header Updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,11 +4,16 @@ const Button = ({ className, href, onClick, children, px, white }) => {
   const classes = `button relative inline-flex  items-center justify-center h-11 transition-colos hover:text-colo-1 ${
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${className || " "}`;
+
+  const spanClasses = "relative z-10";
+
   const renderButton = () => (
     <button className={classes}>
-      <span>{children}</span>
+      <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
+
+    
   );
   return renderButton();
 };
